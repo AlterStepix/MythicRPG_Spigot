3c0b3aec68feb89e2e3a1c4f5c2b9aa8c628fa6b
Crutogurch <crutogurch@yandex.ru>
2022-03-05 20:54:01 +0500

    creepersetPowered

--- src/main/java/alterstepix/mythicrpg/Events.java
+++ src/main/java/alterstepix/mythicrpg/Events.java
@@ -1,8 +1,10 @@
 package alterstepix.mythicrpg;
 
+import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.util.Vector;
 
@@ -14,6 +16,11 @@ public class Events implements Listener {
         Player player = (Player)e.getBreeder();
         Vector vector = new Vector(0,1,0);
         player.setVelocity(vector);
+    }
+    @EventHandler
+    public void CreatureSpawnEvent(CreatureSpawnEvent event){
+        Creeper creeper = (Creeper)event.getEntity();
+        creeper.setPowered(true);
     }
 
 }

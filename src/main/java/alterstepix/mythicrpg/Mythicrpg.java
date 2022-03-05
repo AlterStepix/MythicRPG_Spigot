f5075d03ad75e3756e23d2a468eca397c188fb8b
Stepix <darklightgamesz@gmail.com>
2022-03-05 20:56:26 +0500

    Added config, ItemManager

--- src/main/java/alterstepix/mythicrpg/Mythicrpg.java
+++ src/main/java/alterstepix/mythicrpg/Mythicrpg.java
@@ -1,20 +1,30 @@
 package alterstepix.mythicrpg;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Mythicrpg extends JavaPlugin {
 
+    FileConfiguration config = getConfig();
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        config.options().copyDefaults(true);
+        saveConfig();
+
         Bukkit.getServer().getPluginManager().registerEvents(new Events(), this);
         Bukkit.getLogger().info("[MRPG] Plugin Enabled");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
         Bukkit.getLogger().info("[MRPG] Plugin Disabled");
+    }
+
+    public FileConfiguration getConfig()
+    {
+        return config;
     }
 }
---
+++ src/main/java/alterstepix/mythicrpg/util/ItemManager.java
@@ -0,0 +1,40 @@
+package alterstepix.mythicrpg.util;
+
+import alterstepix.mythicrpg.Mythicrpg;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemManager {
+    public static ItemStack LightingAxe;
+    static Mythicrpg main;
+
+    public void init(Mythicrpg main)
+    {
+        this.main = main;
+    }
+
+    public static void createLightingAxe()
+    {
+        ItemStack item = new ItemStack(Material.IRON_AXE, 1);
+        ItemMeta meta = item.getItemMeta();
+
+        meta.setDisplayName(main.getConfig().getString("lightning_axe_name"));
+        meta.setUnbreakable(true);
+        meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        List<String> lore = new ArrayList<>();
+        lore.add("§6RIGHT CLICK: §eThunderlord");
+        lore.add("§7Strikes all nearby enemies with lightning");
+
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+
+        LightingAxe = item;
+
+    }
+
+}

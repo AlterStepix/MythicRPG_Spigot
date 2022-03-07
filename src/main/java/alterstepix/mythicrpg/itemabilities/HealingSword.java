812b13e16aaeee69e7c7d27c099d21e1c61559fb
Crutogurch <crutogurch@yandex.ru>
2022-03-07 09:30:10 +0500

    HealingSwordF

---
+++ src/main/java/alterstepix/mythicrpg/itemabilities/HealingSword.java
@@ -0,0 +1,34 @@
+package alterstepix.mythicrpg.itemabilities;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class HealingSword implements Listener {
+    @EventHandler
+    public void onInteract(PlayerInteractEvent e)
+    {
+        if(e.getAction().equals(Action.RIGHT_CLICK_AIR) || e.getAction().equals(Action.RIGHT_CLICK_BLOCK))
+        {
+            Player player = e.getPlayer();
+
+
+            if(player.getInventory().getItemInMainHand().getItemMeta() != null && player.getInventory().getItemInMainHand().getItemMeta().getLore() != null && player.getInventory().getItemInMainHand().getItemMeta().getLore().contains("§6RIGHT CLICK: §eHealing")) {
+                int hunger = player.getFoodLevel();
+
+                if(hunger>0) //Hunger > 0
+                {
+                    player.setHealth(20);
+                    player.setFoodLevel(hunger-10);
+                }
+
+
+
+            }
+        }
+    }
+
+}

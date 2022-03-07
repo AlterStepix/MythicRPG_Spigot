e7e99bd56eed0233cedc3a8ac3bb3c9ecadddbe9
Crutogurch <crutogurch@yandex.ru>
2022-03-07 21:17:29 +0500

    Thunderlord

--- src/main/java/alterstepix/mythicrpg/commands/AppendAbilityLore.java
+++ src/main/java/alterstepix/mythicrpg/commands/AppendAbilityLore.java
@@ -6,6 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -17,11 +18,13 @@ public class AppendAbilityLore implements CommandExecutor {
 
     Mythicrpg main;
     ItemManager m;
+    FileConfiguration config;
     public AppendAbilityLore(Mythicrpg main)
     {
         this.main = main;
         m = new ItemManager(main);
         m.init();
+        this.config = main.getConfiguration();
     }
 
     @Override
@@ -30,18 +33,30 @@ public class AppendAbilityLore implements CommandExecutor {
         if(args.length == 1)
         {
             Player p = (Player)sender;
+            ItemStack mainhand = p.getInventory().getItemInMainHand();
+            ItemMeta meta = mainhand.getItemMeta();
+            List<String> lore = new ArrayList<String>();
             switch(args[0])
             {
                 case "Curse":
-                    ItemStack mainhand = p.getInventory().getItemInMainHand();
-                    ItemMeta meta = mainhand.getItemMeta();
-                    List<String> lore = new ArrayList<String>();
                     if(meta.hasLore())
                         lore=meta.getLore();
 
                     lore.add("");
                     lore.add("§6ITEM ABILITY: §eCurse");
                     lore.add("§7Applies strong debuffs to your enemies on hit");
+                    meta.setLore(lore);
+                    mainhand.setItemMeta(meta);
+                    p.getInventory().setItemInMainHand(mainhand);
+                    break;
+                case "Thunderlord":
+                    if(meta.hasLore())
+                        lore=meta.getLore();
+                    int cooldown = this.config.getInt("healingSwordCooldown");
+                    lore.add("");
+                    lore.add("§6RIGHT CLICK: §eThunderlord");
+                    lore.add("§7Strikes all nearby enemies with lightning");
+                    lore.add("§8Cooldown: "+ cooldown + "s");
                     meta.setLore(lore);
                     mainhand.setItemMeta(meta);
                     p.getInventory().setItemInMainHand(mainhand);

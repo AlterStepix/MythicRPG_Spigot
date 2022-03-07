6b44358352d72fb7b61359b8c6892994ce13093c
Crutogurch <crutogurch@yandex.ru>
2022-03-07 19:36:11 +0500

    AppendAbilityLore not complete

--- src/main/java/alterstepix/mythicrpg/commands/AppendAbilityLore.java
+++ src/main/java/alterstepix/mythicrpg/commands/AppendAbilityLore.java
@@ -7,7 +7,20 @@ import org.bukkit.command.CommandSender;
 public class AppendAbilityLore implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        if(args.length == 1)
+        {
+            if (args[0].toLowerCase()=="curse")
+            {
+
+
+            }
+
+            return true;
+        }
+
+
         return false;
     }
 }

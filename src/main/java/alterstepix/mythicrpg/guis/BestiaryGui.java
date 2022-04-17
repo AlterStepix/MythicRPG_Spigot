00b3cff9ed51ad9b6e8d18f6299ec31ac569f0ae
Crutogurch <crutogurch@yandex.ru>
2022-04-17 21:12:02 +0500

    Added more mobs to BestiaryGui

--- src/main/java/alterstepix/mythicrpg/guis/BestiaryGui.java
+++ src/main/java/alterstepix/mythicrpg/guis/BestiaryGui.java
@@ -50,8 +50,8 @@ public class BestiaryGui implements Listener {
         ArrayList<String> ParasiteDrops = new ArrayList<>();
         ParasiteDrops.add(ColorUtil.ConvertToCustom(config.getString("RareRarity"))+": "+ColorUtil.ConvertToCustom(config.getString("ParasiteHeartName"))+" with "+config.getInt("RareChance")+"%"+" chance");
         ParasiteDrops.add("§7Other:");
-        ParasiteDrops.add("§730-th level armor with 30% chance");
-        ParasiteDrops.add("§730-th level sword with 30% chance");
+        ParasiteDrops.add("§730-th level armor with 50% chance");
+        ParasiteDrops.add("§730-th level sword with 50% chance");
         ItemStack ParasitePage = builder.create(config.getString("ParasiteNametag"),10,config.getInt("ParasiteHealth"),ParasiteDesc,ParasiteDrops);
 
         // Infected Zombie
@@ -78,11 +78,53 @@ public class BestiaryGui implements Listener {
         AirSpiritDrops.add(ColorUtil.ConvertToCustom(config.getString("CommonRarity"))+": "+ColorUtil.ConvertToCustom(config.getString("ImpulseShardName"))+" with "+config.getInt("CommonChance")+"%"+" chance");
         ItemStack AirSpiritPage = builder.create(config.getString("AirSpiritNametag"),5,config.getInt("AirSpiritHealth"),AirSpiritDesc,AirSpiritDrops);
 
+        // Fire Spirit
+        ArrayList<String> FireSpiritDesc = new ArrayList<>();
+        FireSpiritDesc.add("§7This is dangerous mob summoned by a "+config.getString("SemiIdolNametag").split("!")[1]+".");
+        FireSpiritDesc.add("§7It also ignites opponent for 6 seconds.");
+        ArrayList<String> FireSpiritDrops = new ArrayList<>();
+        FireSpiritDrops.add(ColorUtil.ConvertToCustom(config.getString("CommonRarity"))+": "+ColorUtil.ConvertToCustom(config.getString("AmberShardName"))+" with "+config.getInt("CommonChance")+"%"+" chance");
+        ItemStack FireSpiritPage = builder.create(config.getString("FireSpiritNametag"),5,config.getInt("FireSpiritHealth"),FireSpiritDesc,FireSpiritDrops);
+
+        // Ice Spirit
+        ArrayList<String> IceSpiritDesc = new ArrayList<>();
+        IceSpiritDesc.add("§7This is dangerous mob summoned by a "+config.getString("SemiIdolNametag").split("!")[1]+".");
+        IceSpiritDesc.add("§7It also gives the opponent a weakness and a slow.");
+        ArrayList<String> IceSpiritDrops = new ArrayList<>();
+        IceSpiritDrops.add(ColorUtil.ConvertToCustom(config.getString("CommonRarity"))+": "+ColorUtil.ConvertToCustom(config.getString("FrozenShardName"))+" with "+config.getInt("CommonChance")+"%"+" chance");
+        ItemStack IceSpiritPage = builder.create(config.getString("IceSpiritNametag"),5,config.getInt("IceSpiritHealth"),IceSpiritDesc,IceSpiritDrops);
+
+        //Semi Idol
+        ArrayList<String> SemiIdolDesc = new ArrayList<>();
+        SemiIdolDesc.add("§7This is mob that summons: ");
+        SemiIdolDesc.add((config.getString("AirSpiritNametag").split("!"))[1]+"s, "+(config.getString("FireSpiritNametag").split("!"))[1]+"s and "+(config.getString("IceSpiritNametag").split("!"))[1]+"s.");
+        SemiIdolDesc.add("§7It also summons lighting bolts at the opponent.");
+        ArrayList<String> SemiIdolDrops = new ArrayList<>();
+        SemiIdolDrops.add(ColorUtil.ConvertToCustom(config.getString("EpicRarity"))+": "+ColorUtil.ConvertToCustom(config.getString("LightningShardName"))+" with "+config.getInt("EpicChance")+"%"+" chance");
+        SemiIdolDrops.add("§7Other:");
+        SemiIdolDrops.add("§740-th level armor with 50% chance");
+        SemiIdolDrops.add("§740-th level sword with 50% chance");
+        ItemStack SemiIdolPage = builder.create(config.getString("SemiIdolNametag"),12,config.getInt("SemiIdolHealth"),SemiIdolDesc,SemiIdolDrops);
+
+        // Ancient Zombie
+        ArrayList<String> AncientZombieDesc = new ArrayList<>();
+        AncientZombieDesc.add("§7It also gives the opponent a weakness.");
+        ArrayList<String> AncientZombieDrops = new ArrayList<>();
+        AncientZombieDrops.add(ColorUtil.ConvertToCustom(config.getString("CommonRarity"))+": "+ColorUtil.ConvertToCustom(config.getString("AncientShardName"))+" with "+config.getInt("CommonChance")+"%"+" chance");
+        AncientZombieDrops.add(ColorUtil.ConvertToCustom(config.getString("RareRarity"))+": "+ColorUtil.ConvertToCustom(config.getString("DecayedHeartName"))+" with "+config.getInt("RareChance")+"%"+" chance");
+        ItemStack AncientZombiePage = builder.create(config.getString("AncientZombieNametag"),3,config.getInt("AncientZombieHealth"),AncientZombieDesc,AncientZombieDrops);
+
         gui.addItem(WitherSpiderPage);
         gui.addItem(ParasitePage);
         gui.addItem(InfectedZombiePage);
         gui.addItem(MasterAssassinPage);
         gui.addItem(AirSpiritPage);
+        gui.addItem(FireSpiritPage);
+        gui.addItem(IceSpiritPage);
+        gui.addItem(SemiIdolPage);
+        gui.addItem(AncientZombiePage);
+
+
         player.openInventory(gui);
     }
 
